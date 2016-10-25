6a32c4568809c74fe38d589cc9c7e5064551f5b0
Oliveck10 <oliveck10@gmail.com>
2016-10-26 04:39:54 +0800

    create entry point for react

---
+++ react-babelJs/src/entry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import TodoApp from './todoapp';
+import 'style/style.css';
+
+render(<TodoApp />, document.getElementById('root'));
+render(
+	<footer className="info">
+		<p> Sourced from  <a href="http://todomvc.com">TodoMVC</a></p>
+	</footer>, document.getElementById('footer')
+);
