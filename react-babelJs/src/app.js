4cd3d9905262e00373eb3c8d76affc29198ff540
Oliveck10 <oliveck10@gmail.com>
2016-10-26 13:18:39 +0800

    add new property to new generated todo-list

--- react-babelJs/src/app.js
+++ react-babelJs/src/app.js
@@ -125,3 +125,14 @@ class TodoList extends React.Component {
     </ul>
   }
 }
+
+class ListItem extends React.Component {
+  render() {
+    return <li className={this.props.removed ? "removed": (this.props.checked ? "completed" : "")}>
+      <div className="view">
+        <input className="toggle" type="checkbox" checked={this.props.checked} onClick={this.props.toggle}/>
+        <label>{this.props.label}</label>
+        <button className="destroy" onClick={this.props.remove} ></button>
+      </div></li>
+  }
+}
